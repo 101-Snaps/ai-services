f19d0aadd5eb23ac41d3c9e1100805ebc3d8f698
101-Snaps <makhafola.101snaps@gmail.com>
2026-05-04 21:07:43 +0200

    Change database name from 'cyber_intel' to 'railway'

--- app.py
+++ app.py
@@ -167,9 +167,10 @@ model = load_or_train_model()
 def get_db_connection():
     return mysql.connector.connect(
         host=os.getenv("DB_HOST", "localhost"),
+        port=int(os.getenv("DB_PORT", 3306)),
         user=os.getenv("DB_USER", "root"),
         password=os.getenv("DB_PASSWORD", ""),
-        database=os.getenv("DB_NAME", "cyber_intel")
+        database=os.getenv("DB_NAME", "railway")
     )
 
 
